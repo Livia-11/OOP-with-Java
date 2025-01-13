b3d918c036b75ada29a417e22d35ffa49817e57b
Livia-11 <kirezilivia@gmail.com>
2025-01-13 09:43:25 +0200

    New Enum day running in public class program with class public class gender

---
+++ src/app/oop/enums/Day.java
@@ -0,0 +1,11 @@
+package app.oop.enums;
+
+public enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday;
+}
--- src/app/oop/enums/Program.java
+++ src/app/oop/enums/Program.java
@@ -3,11 +3,14 @@ package app.oop.enums;
 
 public class Program {
 
-    enum Gender{
-        MALE,FEMALE;
-    }
     public static void main(String[] args) {
         Gender gender = Gender.FEMALE;
+        Day day = Day.Friday;
+
+        System.out.println(day);
+        System.out.println(day.name());
+        System.out.println(day.ordinal());
+
         System.out.println(gender);
         System.out.println(gender.name());
         System.out.println(gender.ordinal());
