e79643fb1022d0b9a81a74fc95576a7871f6c136
Livia-11 <kirezilivia@gmail.com>
2025-01-13 09:50:21 +0200

    Classifying each week with it's corresponding view

--- src/app/oop/enums/Day.java
+++ src/app/oop/enums/Day.java
@@ -8,4 +8,30 @@ public enum Day {
     Friday,
     Saturday,
     Sunday;
+
+    public void information(){
+        switch(this){
+            case Monday:
+                System.out.println("Checking out the week");
+                break;
+                case Tuesday:
+                    System.out.println("Viewing the week");
+                    break;
+                    case Wednesday:
+                        System.out.println("Dive into work");
+                        break;
+                        case Thursday:
+                            System.out.println("More hardwork to finish the week safe");
+                            break;
+                            case Friday:
+                                System.out.println("Wrapping up to go party for the weekend");
+                                break;
+                                case Saturday:
+                                    System.out.println("Spend the whole day resting and partying during the night");
+                                    break;
+                                    case Sunday:
+                                        System.out.println("Going to church and planning for the next week");
+                                        break;
+        }
+    }
 }
