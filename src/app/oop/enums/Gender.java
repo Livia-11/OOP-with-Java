dddd129e281e945b6331654e7573ed9de823c7b5
Livia-11 <kirezilivia@gmail.com>
2025-01-13 09:38:19 +0200

    adding the enum and program in one class

--- src/app/oop/enums/Gender.java
+++ src/app/oop/enums/Gender.java
@@ -1,5 +1,5 @@
-package app.oop.enums;
-
-public enum Gender {
-    MALE, FEMALE;
-}
+//package app.oop.enums;
+//
+//public enum Gender {
+//    MALE, FEMALE;
+//}
--- src/app/oop/enums/Program.java
+++ src/app/oop/enums/Program.java
@@ -1,9 +1,14 @@
 package app.oop.enums;
 
+enum Gender{
+    MALE,FEMALE;
+}
 public class Program {
 
     public static void main(String[] args) {
         Gender gender = Gender.FEMALE;
+        System.out.println(gender);
         System.out.println(gender.name());
+        System.out.println(gender.ordinal());
     }
 }
